55f2128173806842bef1f92090478b509bf7c306
bhavBains <deepbains26@gmail.com>
2017-11-09 07:28:40 +0000

    created a register page

--- express_servers.js
+++ express_servers.js
@@ -1,7 +1,7 @@
 // Requiring necessary packages for the app
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); 
 const cookieParser = require('cookie-parser');
 
 var PORT = process.env.PORT || 8080; // default port 8080
@@ -22,14 +22,16 @@ app.use(bodyParser.urlencoded({extended: true})); // To parse the form data
 
 app.set("view engine", "ejs"); // to use the ejs templates in app
 
+// Database for URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 // Home Page @ 127.0.0.1/
 // Add a rendered Home page (better looking obviously) later
-// Will add a form eventually to home page to look up fo rurl and shorten it
 app.get("/", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -40,6 +42,16 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
 	let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars)
+});
+
+// Registraion Page- GET and POST
+app.get("/urls/register", (req, res) =>{
+  let templateVars = { username : req.cookies["username"]};
+  res.render("urls_register", templateVars);
+});
+
+app.post("/urls/register", (req,res)=>{
+  res.redirect("/urls/register")
 });
 
 //new page NEW and renders the urls_new page
